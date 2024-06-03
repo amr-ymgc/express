b896a096446d9e3184e1b79b9e0c54b9692b5b4a
amr-ymgc <warashubon@gmail.com>
2024-06-04 00:35:13 +0900

    fix

--- routes/notes.js
+++ routes/notes.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 // 接続情報を設定
 const { MongoClient } = require("mongodb"); 
-const uri ="mongodb+srv://amr-ymgc:shikishiki16@cluster0.apheoxt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri ="****";
 const client = new MongoClient(uri);
 
 router.get('/', async (req, res) => {

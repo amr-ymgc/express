b527cbc52434d0953ccd4142434d840c4b3a8b08
amr-ymgc <warashubon@gmail.com>
2024-07-28 23:34:04 +0900

    fixxx

--- routes/notes_from_b.js
+++ routes/notes_from_b.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 // 接続情報を設定
 const { MongoClient } = require("mongodb");
-const uri = mongodb+srv://amr-ymgc:shikishiki16@cluster0.apheoxt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0;
+const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri);
 
 // corsミドルウェアを使用
